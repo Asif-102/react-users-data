2e6d07f77bb7dcee83d1f2c3df681cbfcc44347f
Asif2535 <aiqbal517@gmail.com>
2021-02-25 18:02:01 +0600

    practice splice to set range

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [persons, setPersons] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data =>setPersons(data))
+    // .then(data =>setPersons(data.splice(0,5))) <==for taking 5 users only
+  },[])
   return (
     <div className="App">
+      {
+        persons.map(person => <Person key={person.id} fullName={person.name} Nickname={person.username} email={person.email}></Person>)
+      }
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
+}
+
+function Person(props){
+  const personStyle={
+    width:'300px',
+    height:'300px',
+    border:'2px solid lightblue',
+    backgroundColor:'lightgray',
+    margin:'10px',
+    display:'inline-block'
+  }
+  return (
+    <div style={personStyle}>
+      <h4>Full-Name: {props.fullName}</h4>
+      <h3>Nick-Name: {props.Nickname}</h3>
+      <h5>Email-Address: {props.email}</h5>
+    </div>
+  )
 }
 
 export default App;
